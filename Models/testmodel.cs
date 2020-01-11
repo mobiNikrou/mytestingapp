d702db8a5ad87a3bde2dda88acd4c2e7c39c5105
mobiNick <mobin.nikrou@gmail.com>
2020-01-11 23:46:12 +0330

    dynamication form and test detail model

--- Models/testmodel.cs
+++ Models/testmodel.cs
@@ -7,18 +7,19 @@ namespace MyTestingWebApp.Models
     {
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Test Name")]
         public string testname { get; set; }
         [Required]
-
+        [Display(Name = "Test Type")]
         public String testtype { get; set; }
         [Required]
-
+        [Display(Name = "Test Xpath1")]
         public string testxpath1 { get; set; }
         [Required]
-
+        [Display(Name = "Test Xpath2")]
         public string testxpath2 { get; set; }
         [Required]
-
+        [Display(Name = "Test Status")]
         public string teststatus { get; set; }
     }
 }
